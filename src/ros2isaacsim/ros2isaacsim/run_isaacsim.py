b9be99aa2f8c07050e413fcc2cf49ec25512fc9b
Trần Đăng An <63138206+Bigkatoan@users.noreply.github.com>
2024-11-12 16:44:50 +0700

    Update run_isaacsim.py

--- src/ros2isaacsim/ros2isaacsim/run_isaacsim.py
+++ src/ros2isaacsim/ros2isaacsim/run_isaacsim.py
@@ -1,8 +1,11 @@
+# Use the isaacsim to import SimulationApp
 from isaacsim import SimulationApp
 
+# Setting the config for simulation and make an simulation.
 CONFIG = {"renderer": "RayTracedLighting", "headless": False}
 simulation_app = SimulationApp(CONFIG)
-        
+
+# Import dependencies.
 import carb
 import omni
 import omni.graph.core as og
@@ -18,11 +21,19 @@ from rclpy.node import Node
 from isaacsim_msgs.msg import Euler, Quat
 from isaacsim_msgs.srv import ImportUsd
 
+#======================================Base======================================
+# Setting up world and enable ros2_bridge extentions.
 world = World()
 extensions.enable_extension("omni.isaac.ros2_bridge")
-simulation_app.update()
-simulation_context = SimulationContext(stage_units_in_meters=1.0)
+simulation_app.update() #update the simulation once for update ros2_bridge.
+simulation_context = SimulationContext(stage_units_in_meters=1.0) #currently we use 1m for simulation.
 
+# update the simulation.
+def run():
+    simulation_app.update()
+#================================================================================
+#============================usd importer service================================
+# Usd importer (service) -> bool.
 def usd_importer(request, response):
     usd_path = request.usd_path
     prim_path = request.prim_path
@@ -31,9 +42,7 @@ def usd_importer(request, response):
     response.ret = True
     return response 
 
-def run():
-    simulation_app.update()
-
+# Usd importer service callback.
 def ImportUSD():
     node = rclpy.create_node('ImportUSD')
     service = node.create_service(srv_type=ImportUsd, 
@@ -41,7 +50,8 @@ def ImportUSD():
                         callback=usd_importer)
     timer = node.create_timer(1/60, callback=run)
     return node, service
-    
+#=================================================================================
+#======================================main=======================================
 def main(arg=None):
     rclpy.init()
     world.scene.add_default_ground_plane()
@@ -50,6 +60,6 @@ def main(arg=None):
     node.destroy_node()
     rclpy.shutdown()
     return
-
+#=================================================================================
 if __name__ == "__main__":
     main()
