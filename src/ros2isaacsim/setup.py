698c9c93e95c6fe0da878551569355b15094f7b3
bigkatoan <bigkatoan6969@gmail.com>
2025-01-05 15:53:00 +0700

    clean

--- src/ros2isaacsim/setup.py
+++ src/ros2isaacsim/setup.py
@@ -25,8 +25,6 @@ setup(
         'console_scripts': [
             "run_isaacsim=ros2isaacsim.run_isaacsim:main",
             "convert_urdf_usd=ros2isaacsim.convert_urdf_usd:main",
-            "control=ros2isaacsim.control:main",
-            "control2=ros2isaacsim.control2:main",
-        ],
+            ],
     },
 )
